ecdc127422307832095df130bc956b46a698e588
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-06-13 18:11:34 +0900

    add backend

---
+++ back/src/main/java/ITS/BTinside/BTinsideApplication.java
@@ -0,0 +1,13 @@
+package ITS.BTinside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTinsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BTinsideApplication.class, args);
+	}
+
+}
---
+++ back/src/main/java/ITS/BTinside/Controller/UserController.java
@@ -0,0 +1,25 @@
+package ITS.BTinside.Controller;
+
+import ITS.BTinside.Entity.User;
+import ITS.BTinside.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return  userService.getAllUsers();
+    }
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+}
